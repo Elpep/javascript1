b64962acf79f29a620119ab6dc325f7b910358ec
Elias Peppas <44575475+Elpep@users.noreply.github.com>
2019-07-29 16:25:24 +0300

    Add files via upload

---
+++ javascript1_week2/js1_week2.js
@@ -0,0 +1,81 @@
+//Part 1
+console.log("Hello")
+
+//Part 2
+console.log("I'm awesome")
+
+//Part 3
+let x
+console.log("The value of x will be nothing")
+console.log(x)
+x = 5
+console.log("The value of x will be 5")
+console.log(x)
+
+//Part 4
+let y = "Random string"
+console.log("The value of y will be whatever it needs to be")
+console.log(y)
+y = "New random string"
+console.log("The value of y will be the new string")
+console.log(y)
+
+//Part 5
+let z = 7.25
+console.log(z)
+let a = Math.round(z)
+console.log(a)
+if (a > z){
+    console.log(a)
+}else{
+    console.log(z)
+}
+
+//Part 6
+let plurality_array = []
+console.log("Value of the array is going to be nothing")
+console.log(plurality_array)
+let favorite_animals = ["cat", "dog"]
+console.log(favorite_animals)
+favorite_animals.push("baby pig")
+console.log(favorite_animals)
+
+//Part 7
+let myString = "this is a test"
+console.log(myString)
+myString.length
+console.log(myString.length)
+
+//Part 8
+let b = 42
+let c = "hello"
+let d = true
+let e = null
+console.log("The value of my variable x is: " + b)
+console.log("The value of my variable y is: " + c)
+console.log("The value of my variable z is: " + d)
+console.log("The value of my variable a is: " + e)
+console.log("I think the aforementioned variables are of various types")
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+
+/* if (typeof b) === (typeof c){
+     console.log("SAME TYPE")
+    }else{
+     console.log("Not quite so similar")
+    }
+*/
+
+//Part 9
+f = 7
+f = f % 3
+console.log(f)
+
+console.log("The % is used to provide the remainder of a division. Nothing more to add really")
+
+//Part 10
+
+let what_an_array_omg = [5, "wow", true, undefined, null ]
+console.log(what_an_array_omg)
